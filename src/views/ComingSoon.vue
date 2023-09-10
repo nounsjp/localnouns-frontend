284becedf2f357ec0ebf240992d9e827e2e1602b
eibakatsu <eiba.katsu@gmail.com>
2023-09-11 08:31:42 +0900

    apply i18n

--- src/views/ComingSoon.vue
+++ src/views/ComingSoon.vue
@@ -1,13 +1,8 @@
 <template>
   <div class="mx-auto max-w-lg p-2 text-left">
-    <div v-if="lang === 'en'">
-      <p class="mb-2 font-londrina text-3xl">Coming Soon</p>
-      <div class="mb-8 space-y-2 font-pt-root font-medium"></div>
-    </div>
-    <div v-else>
-      <p class="mb-2 font-yusei text-3xl">近日公開</p>
-      <div class="mb-8 space-y-2"></div>
-    </div>
+    <p class="mb-2 font-londrina text-3xl">
+      {{ $t("commingSoon.commingSoon") }}
+    </p>
   </div>
 </template>
 
