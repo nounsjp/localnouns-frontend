d4359a75e7dc2f842c3b7fc1c53c10e92b77d4ba
eibakatsu <eiba.katsu@gmail.com>
2023-10-20 10:05:56 +0900

    format

--- src/components/TokenSaleOrTrade.vue
+++ src/components/TokenSaleOrTrade.vue
@@ -1,27 +1,158 @@
 <template>
   <div
     v-if="isOpen"
-    class="fixed inset-0 z-50 overflow-auto bg-smoke-dark flex justify-center items-center"
+    class="fixed inset-0 z-50 overflow-y bg-smoke-dark flex justify-center items-center"
   >
     <div
-      class="relative p-6 bg-white w-3/4 h-3/4 flex flex-col items-center justify-center"
+      class="relative p-6 bg-white w-3/4 h-4/5 overflow-y-auto flex flex-col justify-between items-center"
     >
+      <span class="absolute top-4 right-4 cursor-pointer">
+        <button
+          @click="closeModal(false)"
+          class="mt-4 inline-block rounded bg-green-500 px-6 py-2.5 leading-tight text-white shadow-md transition duration-150 ease-in-out hover:bg-green-700 hover:shadow-lg focus:bg-green-700 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-green-800 active:shadow-lg"
+        >
+          {{ $t("tokenManagement.close") }}
+        </button>
+      </span>
+
       <TokenDetail :token="token" size="L" />
-      <button @click="closeModal">Close</button>
-      SaleOrTrade
+      <hr class="border-t border-gray-600 my-4 w-full" />
+
+      <p class="mb-2 font-londrina text-3xl">
+        {{ $t("TokenSaleOrTrade.sale") }}
+      </p>
+      <p class="mb-2 font-londrina text-l">
+        {{ $t("TokenSaleOrTrade.saleDescription") }}
+      </p>
+
+      <div class="mt-4 flex flex-col items-center">
+        <InformationDialog
+          :isOpen="displayInformationDialog"
+          :message="informationMessage"
+          @close="closeModal(true)"
+        />
+        <div v-if="!isSaleBusy">
+          <div class="flex justify-center gap-2 w-full">
+            <button
+              v-if="token.salePrice > 0 && account"
+              @click="buyNoun"
+              class="mt-4 inline-block rounded bg-red-500 px-8 py-4 font-londrina text-3xl leading-tight text-white shadow-md transition duration-150 ease-in-out hover:bg-red-700 hover:shadow-lg focus:bg-red-700 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-red-800 active:shadow-lg"
+            >
+              {{ $t("TokenSaleOrTrade.buy") }}
+            </button>
+            <button
+              v-else
+              class="mt-4 inline-block rounded bg-gray-500 px-8 py-4 font-londrina text-3xl leading-tight text-white shadow-md transition duration-150"
+              disabled
+            >
+              {{ $t("TokenSaleOrTrade.buy") }}
+            </button>
+          </div>
+        </div>
+        <button
+          type="button"
+          v-if="isSaleBusy"
+          class="inline-block rounded px-8 py-4 leading-tight text-gray-500 shadow-md"
+          disabled
+        >
+          <img
+            class="absolute h-4 w-10 animate-spin"
+            src="@/assets/red160px.png"
+          />
+          <span class="ml-10">{{ $t("message.processing") }}</span>
+        </button>
+      </div>
+
+      <hr class="border-t border-gray-600 my-4 w-full" />
+
+      <p class="mb-2 font-londrina text-3xl">
+        {{ $t("TokenSaleOrTrade.trade") }}
+      </p>
+      <p class="mb-2 font-londrina text-l">
+        {{ $t("TokenSaleOrTrade.tradeDescription") }}
+      </p>
+      <div>
+        <div class="flex justify-center w-full mt-4">
+          <div class="flex flex-wrap">
+            <div>{{ $t("TokenSaleOrTrade.tradeForPrefecture") }} :</div>
+            <div
+              v-for="(option, index) in tradeForPrefectures"
+              :key="index"
+              class="flex items-center mr-4 mb-2"
+            >
+              <label class="ml-2 font-londrina text-l">{{
+                $t("prefecture." + option)
+              }}</label>
+            </div>
+          </div>
+        </div>
+
+        <div v-if="!isTradeBusy">
+          <div class="flex justify-center gap-2 w-full">
+            <button
+              v-if="token.isOnTrade && account"
+              @click="tradeNoun"
+              class="mt-4 inline-block rounded bg-blue-500 px-8 py-4 font-londrina text-3xl leading-tight text-white shadow-md transition duration-150 ease-in-out hover:bg-red-blue hover:shadow-lg focus:bg-red-blue focus:shadow-lg focus:outline-none focus:ring-0 active:bg-red-blue active:shadow-lg"
+            >
+              {{ $t("TokenSaleOrTrade.trade") }}
+            </button>
+            <button
+              v-else
+              class="mt-4 inline-block rounded bg-gray-500 px-8 py-4 font-londrina text-3xl leading-tight text-white shadow-md transition duration-150"
+              disabled
+            >
+              {{ $t("TokenSaleOrTrade.trade") }}
+            </button>
+          </div>
+        </div>
+        <button
+          type="button"
+          v-if="isTradeBusy"
+          class="inline-block rounded px-8 py-4 leading-tight text-gray-500 shadow-md"
+          disabled
+        >
+          <img
+            class="absolute h-4 w-10 animate-spin"
+            src="@/assets/red160px.png"
+          />
+          <span class="ml-10">{{ $t("message.processing") }}</span>
+        </button>
+      </div>
+
+      <hr class="border-t border-gray-600 my-4 w-full" />
+      <button
+        @click="closeModal(false)"
+        class="mt-4 inline-block rounded bg-green-500 px-6 py-2.5 leading-tight text-white shadow-md transition duration-150 ease-in-out hover:bg-green-700 hover:shadow-lg focus:bg-green-700 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-green-800 active:shadow-lg"
+      >
+        {{ $t("tokenManagement.close") }}
+      </button>
     </div>
   </div>
 </template>
 
 <script>
+import { ref, toRefs, computed } from "vue";
+import { useStore } from "vuex";
+import { useI18n } from "vue-i18n";
+import { ethers } from "ethers";
+import { getLocalNounsTokenContract } from "@/utils/const";
+import { ChainIdMap } from "@/utils/MetaMask";
 import TokenDetail from "./TokenDetail.vue";
+import InformationDialog from "@/components/InformationDialog.vue";
+import { prefectureList } from "@/i18n/prefectures";
+import { addresses } from "@/utils/addresses";
 // import { TOKEN } from "@/firestore/token";
 
 export default {
   components: {
     TokenDetail,
+    InformationDialog,
   },
   props: {
+    network: {
+      type: String,
+      required: true,
+    },
     isOpen: {
       type: Boolean,
       required: true,
@@ -32,12 +163,140 @@ export default {
     },
   },
   setup(props, context) {
-    console.log("token:", props.token);
-    const closeModal = () => {
-      context.emit("close");
+    const store = useStore();
+    const i18n = useI18n();
+
+    const account = computed(() => store.state.account);
+    const { token, network } = toRefs(props);
+    console.log("tokenManagement-network:", network.value);
+    console.log("tokenManagement-token:", token.value);
+
+    const salePrice = ref(""); // 初期値として空文字を設定
+    const isSaleBusy = ref(false);
+    const isTradeBusy = ref(false);
+    const displayInformationDialog = ref(false);
+    const informationMessage = ref("");
+
+    const removeSalePrice = async () => {
+      const contract = await getContract(props.network);
+      isSaleBusy.value = true;
+      try {
+        const txParams = { value: 0 };
+        const tx = await contract.setPriceOf(props.token.tokenId, 0, txParams);
+        const result = await tx.wait();
+        console.log("removeSalePrice:tx", result);
+        isSaleBusy.value = false;
+        informationMessage.value = i18n.t(
+          "tokenManagement.finishRemoveSalePrice",
+        );
+        displayInformationDialog.value = true;
+      } catch (e) {
+        isSaleBusy.value = false;
+        console.error(e);
+      }
     };
+
+    // 都道府県リストの要素番号からキーに変換
+    const tradeForPrefectures = computed(() => {
+      const tradeToPrefecture = token.value?.tradeToPrefecture || [];
+      return tradeToPrefecture.map((index) => prefectureList[index]);
+    });
+    let selectedPrefectures = [];
+
+    const setTrade = async () => {
+      console.log("setTrade:selectedPrefectures", selectedPrefectures);
+      // 入力チェック
+      if (selectedPrefectures.length == 0) {
+        alert(i18n.t("tokenManagement.validSelectPrefectures"));
+        return;
+      }
+
+      const contract = await getContract(props.network);
+
+      // 指定しない(0)は削除
+      selectedPrefectures = selectedPrefectures.filter((item) => item !== 0);
+
+      isTradeBusy.value = true;
+      console.log("selectedPrefectures", selectedPrefectures);
+      try {
+        const weiValue = ethers.parseEther("0.002");
+        console.log("weiValue", weiValue);
+        // const txParams = { value: weiValue };
+        const txParams = { value: 0 };
+        const tx = await contract.putTradeLocalNoun(
+          props.token.tokenId,
+          selectedPrefectures,
+          txParams,
+        );
+        const result = await tx.wait();
+        console.log("putTradeLocalNoun:tx", result);
+        isTradeBusy.value = false;
+        informationMessage.value = i18n.t("tokenManagement.finishSetTrade");
+        displayInformationDialog.value = true;
+      } catch (e) {
+        isTradeBusy.value = false;
+        console.error(e);
+      }
+    };
+
+    const stopTrade = async () => {
+      const contract = await getContract(props.network);
+
+      isTradeBusy.value = true;
+      try {
+        const txParams = { value: 0 };
+        const tx = await contract.cancelTradeLocalNoun(
+          props.token.tokenId,
+          txParams,
+        );
+        const result = await tx.wait();
+        console.log("cancelTradeLocalNoun:tx", result);
+        isTradeBusy.value = false;
+        informationMessage.value = i18n.t("tokenManagement.finishStopTrade");
+        displayInformationDialog.value = true;
+      } catch (e) {
+        isTradeBusy.value = false;
+        console.error(e);
+      }
+    };
+
+    const handleUpdatePrefectures = (prefectures) => {
+      console.log("handleUpdatePrefectures", prefectures);
+      selectedPrefectures = prefectures;
+    };
+
+    const closeModal = (reload) => {
+      console.log("closeModal-reload", reload);
+      isSaleBusy.value = false;
+      isTradeBusy.value = false;
+      displayInformationDialog.value = false;
+      context.emit("close", reload);
+    };
+
+    const getContract = async (network) => {
+      const chainId = ChainIdMap[network];
+      const signer = await store.getters.getSigner(chainId);
+
+      return getLocalNounsTokenContract(
+        addresses["localNounsToken"][network],
+        signer,
+      );
+    };
+
     return {
+      account,
       closeModal,
+      salePrice,
+      removeSalePrice,
+      setTrade,
+      stopTrade,
+      tradeForPrefectures,
+      selectedPrefectures,
+      handleUpdatePrefectures,
+      isSaleBusy,
+      isTradeBusy,
+      displayInformationDialog,
+      informationMessage,
     };
   },
 };
