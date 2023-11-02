50cdb5f32b7d827a942e6a3a409d639183736c58
eibakatsu <eiba.katsu@gmail.com>
2023-11-02 09:35:08 +0900

    typo

--- src/views/List.vue
+++ src/views/List.vue
@@ -156,7 +156,7 @@ export default defineComponent({
             return doc.data();
           });
         } catch (e) {
-          console.error("getTokenList", e);
+          console.error("getMyTokenList", e);
         }
       }
     };
