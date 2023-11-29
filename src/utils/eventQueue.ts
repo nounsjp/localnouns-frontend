45029d78d2413331e0f5317465713aeb4a99d804
eibakatsu <eiba.katsu@gmail.com>
2023-11-29 14:44:20 +0900

    format

--- src/utils/eventQueue.ts
+++ src/utils/eventQueue.ts
@@ -1,37 +1,36 @@
-
 type TransferEvent = {
-    from: string;
-    to: string;
-    tokenId: BigInt;
-    event: any;
-  };
-  
+  from: string;
+  to: string;
+  tokenId: BigInt;
+  event: any;
+};
+
 type EventHandler = (event: TransferEvent) => Promise<void>;
 
 export class EventQueue {
-    private queue: { event: TransferEvent; handler: EventHandler }[];
-    private processing: boolean;
-  
-    constructor() {
-      this.queue = [];
-      this.processing = false;
+  private queue: { event: TransferEvent; handler: EventHandler }[];
+  private processing: boolean;
+
+  constructor() {
+    this.queue = [];
+    this.processing = false;
+  }
+
+  private async processNext(): Promise<void> {
+    if (this.processing || this.queue.length === 0) {
+      return;
     }
-  
-    private async processNext(): Promise<void> {
-      if (this.processing || this.queue.length === 0) {
-        return;
-      }
-  
-      this.processing = true;
-      const { event, handler } = this.queue.shift()!;
-      await handler(event);
-      this.processing = false;
-  
-      this.processNext();
-    }
-  
-    public add(event: TransferEvent, handler: EventHandler): void {
-      this.queue.push({ event, handler });
-      this.processNext();
-    }
-  };
+
+    this.processing = true;
+    const { event, handler } = this.queue.shift()!;
+    await handler(event);
+    this.processing = false;
+
+    this.processNext();
+  }
+
+  public add(event: TransferEvent, handler: EventHandler): void {
+    this.queue.push({ event, handler });
+    this.processNext();
+  }
+}
