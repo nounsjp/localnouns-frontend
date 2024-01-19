c02e2db89af6d067bf24b537ceba1451544daf60
Eiba <eiba.katsu@gmail.com>
2024-01-19 10:01:56 +0900

    表示修正

--- src/views/Top.vue
+++ src/views/Top.vue
@@ -1,6 +1,6 @@
 <template>
-  <div class="mx-auto p-2 text-center font-londrina font-yusei">
-    <img src="/banner.jpeg" class="mb-8 mt-8 w-full" />
+  <div class="mx-auto p-2 mb-200 text-center font-londrina font-yusei">
+    <img src="/banner.jpeg" class="mb-8 mt-8 w-full md:w-4/5 mx-auto" />
     <div v-if="lang === 'en'">
       <div class="text-3xl">
         <p>Someone else's miss</p>
@@ -21,21 +21,46 @@
       <div class="mt-8 mb-16 text-3xl">
         <p>Love for the local community</p>
         <p>Exchangeable</p>
-        <p>Unique Marketplace</p>
+        <p>Full On-Chain</p>
       </div>
-      <div class="mt-8 mb-8 text-3xl underline">
-        <router-link :to="localizedUrl('/mint')"> >>> Go to Mint</router-link>
-      </div>
-      <div class="mt-8 mb-8 text-3xl underline">
-        <router-link :to="localizedUrl('/list/0')">
-          >>> Go to Sale,Trade</router-link
+
+      <div class="mt-8 mb-8">
+        <button
+          class="inline-block rounded bg-green-600 px-6 py-2.5 leading-tight text-white text-3xl shadow-md transition duration-150 ease-in-out hover:bg-green-700 hover:shadow-lg focus:bg-green-700 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-green-800 active:shadow-lg"
         >
+          <router-link :to="localizedUrl('/mint')"> >>> Mint Page</router-link>
+        </button>
+      </div>
+      <div class="mt-8 mb-8">
+        <button
+          class="inline-block rounded bg-green-600 px-6 py-2.5 leading-tight text-white text-3xl shadow-md transition duration-150 ease-in-out hover:bg-green-700 hover:shadow-lg focus:bg-green-700 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-green-800 active:shadow-lg"
+        >
+          <router-link :to="localizedUrl('/list/0')">
+            >>> Marketplace</router-link
+          >
+        </button>
+      </div>
+      <div>
+        <p class="mt-8 mb-2 text-5xl">WTF?</p>
+        <p class="text-xl">
+          Local Nouns is a NFT with full-on chain of dots depicting famous
+          places and specialties from all over Japan!
+        </p>
+        <p class="mt-2 text-xl">
+          If you get a Nouns of your locality, you win, of course!
+        </p>
+        <p class="mt-2 text-xl">
+          If you get a Nouns from an unfamiliar locality, it will be a winner
+          for someone else!
+        </p>
+        <p class="mt-2 mb-2 text-xl underline">
+          <router-link :to="localizedUrl('/about')"> >>> more...</router-link>
+        </p>
       </div>
     </div>
     <div v-else>
       <div class="text-3xl">
-        <p>誰かのハズレが</p>
-        <p>誰かの当たり</p>
+        <p>誰かのハズレが 誰かの当たり</p>
       </div>
       <img
         class="h-3 w-8 animate-spin mx-auto mt-8 mb-8"
@@ -49,17 +74,41 @@
         src="@/assets/red160px.png"
       />
       <div class="mt-8 mb-16 text-3xl">
-        <p>郷土愛・交換可能・独自マケプレ</p>
+        <p>郷土愛・交換可能・フルオンチェーン</p>
       </div>
-      <div class="mt-8 mb-8 text-3xl underline">
-        <router-link :to="localizedUrl('/mint')">
-          >>> ミントページはこちら</router-link
+      <div class="mt-8 mb-8">
+        <button
+          class="inline-block rounded bg-green-600 px-6 py-2.5 leading-tight text-white text-3xl shadow-md transition duration-150 ease-in-out hover:bg-green-700 hover:shadow-lg focus:bg-green-700 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-green-800 active:shadow-lg"
         >
+          <router-link :to="localizedUrl('/mint')">
+            >>> ミントページ</router-link
+          >
+        </button>
       </div>
-      <div class="mt-8 mb-8 text-3xl underline">
-        <router-link :to="localizedUrl('/list/0')">
-          >>> 購入・交換はこちら</router-link
+      <div class="mt-8 mb-8">
+        <button
+          class="inline-block rounded bg-green-600 px-6 py-2.5 leading-tight text-white text-3xl shadow-md transition duration-150 ease-in-out hover:bg-green-700 hover:shadow-lg focus:bg-green-700 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-green-800 active:shadow-lg"
         >
+          <router-link :to="localizedUrl('/list/0')">
+            >>> マーケット</router-link
+          >
+        </button>
+      </div>
+      <div>
+        <p class="mt-16 mb-2 text-5xl">WTF?</p>
+        <p class="text-xl">
+          ご当地Nounsは日本各地の名所や特産品をフルオンチェーンのドット絵で表現した、交換できるNFT
+        </p>
+        <p class="mt-2 text-xl">自分のご当地のNounsが出ればもちろん当たり!</p>
+        <p class="mt-2 text-xl">
+          縁遠いご当地のNounsが出たら、それは別の誰かの当たり!
+        </p>
+        <p class="mt-2 text-2xl">
+          さぁ、交換しよう!!
+        </p>
+        <p class="mt-2 mb-2 text-xl underline">
+          <router-link :to="localizedUrl('/about')"> >>> more...</router-link>
+        </p>
       </div>
     </div>
   </div>
