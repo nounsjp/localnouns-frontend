ec773dfd388f7d0cff986af52a2d4f1307d475eb
Eiba <eiba.katsu@gmail.com>
2024-02-01 08:10:39 +0900

    P2Pセール、P2Pトレード成立時にdiscordへポスト

--- server/script/monitoringEvents.ts
+++ server/script/monitoringEvents.ts
@@ -5,15 +5,18 @@ import { ALCHEMY_API_KEY, NETWORK, DISCORD_ANNOUNCE_CHANNEL_ID } from "@/config/
 import {
   getProvider,
   getLocalNounsTokenContract,
+  getNNSENSReverseResolverContract,
+  resolveNNSorENS,
 } from "@/utils/const";
 import { addresses } from "@/utils/addresses";
 import { ethers } from "ethers";
 import { writeTokenDataToFirestore, updateHolderOfTokenOnFirestore, updatePriceOfTokenOnFirestore, updateTradeOfTokenOnFirestore } from "./tokenOnFirestore";
+import { writeHistoryToFirestore } from "./historyOnFirestore";
 import { getTokenInfoAtMint } from "./tokenOnContract";
-import { TOKEN } from "@/firestore/const";
+import { TOKEN, HISTORY, ACTION } from "@/firestore/const";
 import { EventQueue } from "@/utils/eventQueue";
 import { DiscordBot } from "@/utils/DiscordBot";
-import { postForMint, postForPutSale, postForPutTrade } from "./postToDiscord";
+import { postForMint, postForPutSale, postForPutTrade, postForPurchase, postForExecuteTrade } from "./postToDiscord";
 require('dotenv').config();
 
 const provider = getProvider(NETWORK, ALCHEMY_API_KEY);
@@ -21,16 +24,17 @@ const tokenContract = getLocalNounsTokenContract(
   addresses.localNounsToken[NETWORK],
   provider,
 );
+const resolverContract = getNNSENSReverseResolverContract(provider);
 
-const tokenForBot = process.env.DISCORD_BOT_TOKEN ? process.env.DISCORD_BOT_TOKEN:'';
+const tokenForBot = process.env.DISCORD_BOT_TOKEN ? process.env.DISCORD_BOT_TOKEN : '';
 const bot = new DiscordBot(tokenForBot);
 bot.login()
-   .then(() => console.log('Discord Bot Logged in!'))
-   .catch(console.error);
+  .then(() => console.log('Discord Bot Logged in!'))
+  .catch(console.error);
 
 const eventQueue = new EventQueue(3);
 // Transferイベントの監視
-tokenContract.on("Transfer", async (from, to, tokenId, event) => {
+tokenContract.on("Transfer", async (from, to, tokenId, event: any) => {
   // 同時実行スレッドの数を制限するためキューイングして実行する
   eventQueue.add({ from, to, tokenId, event }, async (event) => {
     try {
@@ -40,6 +44,26 @@ tokenContract.on("Transfer", async (from, to, tokenId, event) => {
         tokenInfo.holder = to.toLowerCase();
         // firestoreに書き込み
         await writeTokenDataToFirestore(tokenInfo);
+
+        // 履歴を書き込み
+        const transactionHash = event.event.log.transactionHash;
+        const blockNumber = event.event.log.blockNumber;
+        const block = await provider.getBlock(blockNumber);
+        const timestamp = block?.timestamp;
+
+        const history: HISTORY = {
+          hash: transactionHash,
+          blocknumber: blockNumber,
+          action: ACTION.MINT,
+          tokenId: tokenId,
+          from: from,
+          to: to,
+          fromName: await resolveNNSorENS(resolverContract, from),
+          toName: await resolveNNSorENS(resolverContract, to),
+          timestamp: timestamp ? new Date(timestamp * 1000) : new Date(0)
+        };
+        await writeHistoryToFirestore(history);
+
 
         // discordへポスト
         await postForMint(bot, DISCORD_ANNOUNCE_CHANNEL_ID, tokenId);
@@ -105,5 +129,118 @@ tokenContract.on("CancelTradePrefecture", async (tokenId, event) => {
     console.error("Error:", error);
   }
 });
+
+// Purchaseイベントの監視
+tokenContract.on("Purchase", async (tokenId, _buyer, event) => {
+  try {
+
+    const transactionHash = event.log.transactionHash;
+    const blockNumber = event.log.blockNumber;
+    console.log("transactionHash", transactionHash);
+    console.log("blockNumber", blockNumber);
+
+    // discordへポスト
+    await postForPurchase(bot, DISCORD_ANNOUNCE_CHANNEL_ID, tokenId);
+
+    // 履歴を書き込み
+    const block = await provider.getBlock(blockNumber);
+    const timestamp = block?.timestamp;
+
+    const from = await getTransferFromForTransaction(transactionHash);
+
+    const history: HISTORY = {
+      hash: transactionHash,
+      blocknumber: blockNumber,
+      action: ACTION.PURCHASE,
+      tokenId: tokenId,
+      from: from,
+      to: _buyer,
+      fromName: await resolveNNSorENS(resolverContract, from),
+      toName: await resolveNNSorENS(resolverContract, _buyer),
+      timestamp: timestamp ? new Date(timestamp * 1000) : new Date(0)
+    };
+    await writeHistoryToFirestore(history);
+
+    console.log(`Purchase, TokenID: ${tokenId}`);
+  } catch (error) {
+    console.log("Purchase error:", new Date().toISOString());
+    console.error("Error:", error);
+  }
+});
+
+// // ExecuteTradeイベントの監視
+tokenContract.on("ExecuteTrade", async (targetTokenId, _lister, ownedTokenId, _executer, event) => {
+  try {
+
+    // discordへポスト
+    await postForExecuteTrade(bot, DISCORD_ANNOUNCE_CHANNEL_ID, targetTokenId, ownedTokenId);
+
+    // 履歴を書き込み
+    const transactionHash = event.log.transactionHash;
+    const blockNumber = event.log.blockNumber;
+    console.log("transactionHash", transactionHash);
+    console.log("blockNumber", blockNumber);
+
+    // 履歴を書き込み
+    const block = await provider.getBlock(blockNumber);
+    const timestamp = block?.timestamp;
+
+    // リストしたToken
+    const historyForListed: HISTORY = {
+      hash: transactionHash,
+      blocknumber: blockNumber,
+      action: ACTION.TRADE,
+      tokenId: targetTokenId,
+      from: _executer,
+      to: _lister,
+      fromName: await resolveNNSorENS(resolverContract, _executer),
+      toName: await resolveNNSorENS(resolverContract, _lister),
+      timestamp: timestamp ? new Date(timestamp * 1000) : new Date(0)
+    };
+    await writeHistoryToFirestore(historyForListed);
+
+    // 交換対象のToken
+    const historyForOwned: HISTORY = {
+      hash: transactionHash,
+      blocknumber: blockNumber,
+      action: ACTION.TRADE,
+      tokenId: ownedTokenId,
+      from: _lister,
+      to: _executer,
+      fromName: await resolveNNSorENS(resolverContract, _lister),
+      toName: await resolveNNSorENS(resolverContract, _executer),
+      timestamp: timestamp ? new Date(timestamp * 1000) : new Date(0)
+    };
+    await writeHistoryToFirestore(historyForOwned);
+
+    console.log(`ExecuteTrade, TokenID: ${targetTokenId} <-> ${ownedTokenId}`);
+  } catch (error) {
+    console.log("ExecuteTrade error:", new Date().toISOString());
+    console.error("Error:", error);
+  }
+});
+
+async function getTransferFromForTransaction(transactionHash: string) {
+  // すべてのTransferイベントを取得するためのフィルター
+  const filter = tokenContract.filters.Transfer();
+
+  // フィルタリングされたイベントをクエリ
+  const events = await tokenContract.queryFilter(filter);
+
+  // 特定のトランザクションハッシュに一致するイベントをフィルタリング
+  const matchingEvents = events.filter(event => event.transactionHash === transactionHash);
+
+  // 使用されるのはPurchaseのときだけなので該当するeventは一つのみ
+  for (const event of matchingEvents) {
+    if ("args" in event) {
+      const { from, to, tokenId } = event.args;
+      return from;
+    } else {
+      return '0x9999999999';
+    }
+  }
+  // マッチングするイベントを返す
+  return '0x8888888888';
+}
 
 console.log("monitoringEvents start:", new Date().toISOString());
--- server/script/postToDiscord.ts
+++ server/script/postToDiscord.ts
@@ -76,6 +76,58 @@ ${LOCALNOUNS_URL}/list/${token.prefectureId}
         .catch(console.error);
 } 
 
+export async function postForPurchase(bot: DiscordBot, channelId: string, tokenId: string) {
+    // トークン情報を取得
+    const token: TOKEN = await getTokenInfo(tokenId);
+    const prefectureName = prefecture_ja[token.prefecture as keyof typeof prefecture_ja];
+    const headName = getPartsNameAndDescription('Heads', `${token.prefecture.toLowerCase()}-${token.head.toLowerCase()}`, 'ja')?.name;
+    const accessoryName = getPartsNameAndDescription('Accessories', `${token.prefecture.toLowerCase()}-${token.accessory.toLowerCase()}`, 'ja')?.name;
+
+    // pngファイルに保存
+    const path = await svgStreamToPngFile(token);
+
+    let message = `### ⭐️P2Pセールから購入されました⭐️
+## Local Nouns #${tokenId} ${prefectureName}
+ヘッド    : ${headName}
+アクセサリ: ${accessoryName}
+`;
+    console.log(message);
+    bot.sendMessage(channelId, message, [path])
+        .then(() => console.log('postForPurchase sent!'))
+        .catch(console.error);
+} 
+
+export async function postForExecuteTrade(bot: DiscordBot, channelId: string, ownedTokenId: string, targetTokenId: string) {
+    // トークン情報を取得
+    const ownedToken: TOKEN = await getTokenInfo(ownedTokenId);
+    const ownedPrefectureName = prefecture_ja[ownedToken.prefecture as keyof typeof prefecture_ja];
+    const ownedHeadName = getPartsNameAndDescription('Heads', `${ownedToken.prefecture.toLowerCase()}-${ownedToken.head.toLowerCase()}`, 'ja')?.name;
+    const ownedAccessoryName = getPartsNameAndDescription('Accessories', `${ownedToken.prefecture.toLowerCase()}-${ownedToken.accessory.toLowerCase()}`, 'ja')?.name;
+
+    const targetToken: TOKEN = await getTokenInfo(targetTokenId);
+    const targetPrefectureName = prefecture_ja[targetToken.prefecture as keyof typeof prefecture_ja];
+    const targetHeadName = getPartsNameAndDescription('Heads', `${targetToken.prefecture.toLowerCase()}-${targetToken.head.toLowerCase()}`, 'ja')?.name;
+    const targetAccessoryName = getPartsNameAndDescription('Accessories', `${targetToken.prefecture.toLowerCase()}-${targetToken.accessory.toLowerCase()}`, 'ja')?.name;
+
+    // pngファイルに保存
+    const ownedTokenPath = await svgStreamToPngFile(ownedToken);
+    const targetTokenPath = await svgStreamToPngFile(targetToken);
+
+    let message = `### ⭐️P2Pトレードが成立しました⭐️
+## Local Nouns #${ownedTokenId} ${ownedPrefectureName}
+ヘッド    : ${ownedHeadName}
+アクセサリ: ${ownedAccessoryName}
+   ⇧⇩ ⇧⇩ ⇧⇩
+## Local Nouns #${targetTokenId} ${targetPrefectureName}
+ヘッド    : ${targetHeadName}
+アクセサリ: ${targetAccessoryName}
+`;
+    console.log(message);
+    bot.sendMessage(channelId, message, [ownedTokenPath, targetTokenPath])
+        .then(() => console.log('postForExecuteTrade sent!'))
+        .catch(console.error);
+} 
+
 async function svgStreamToPngFile(token: TOKEN){
     const path = `./tmp/${token.tokenId}.png`;
     const roundedCorners = Buffer.from(token.svg);
